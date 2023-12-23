a8bf729f8cc39b50644cbb2cebfb92a5b4b5c3ea
Raszero <travisjcelli95@gmail.com>
2023-12-23 13:04:39 -0600

    defined columns in product and category models. created get all for category route. secured sequalize connection

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,37 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
+router.get('/', async (req, res) => {
+    // find all categories
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll({include: Product});
+    res.status(200).json(categoryData)
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try{
+  } catch (err) {}
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  try{} catch (err) {}
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  try{} catch (err) {}
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  try{} catch (err) {}
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const routes = require('./routes');
+const sequelize = require('./config/connection')
 // import sequelize connection
+
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,3 +17,15 @@ app.use(routes);
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
+
+// GIVEN a functional Express.js API 
+// WHEN I add my database name, MySQL username, and MySQL password to an environment variable file
+// THEN I am able to connect to a database using Sequelize --- check
+// WHEN I enter schema and seed commands
+// THEN a development database is created and is seeded with test data --- check
+// WHEN I enter the command to invoke the application
+// THEN my server is started and the Sequelize models are synced to the MySQL database --- check 
+// WHEN I open API GET routes in Insomnia for categories, products, or tags
+// THEN the data for each of these routes is displayed in a formatted JSON
+// WHEN I test API POST, PUT, and DELETE routes in Insomnia
+// THEN I am able to successfully create, update, and delete data in my database
