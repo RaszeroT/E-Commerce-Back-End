40470202af76b2d7797ddde0f53b47a162370a53
Raszero <travisjcelli95@gmail.com>
2023-12-23 13:38:54 -0600

    created category put req route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -31,13 +31,16 @@ router.post('/', async (req, res) => {
     const newCategory = await Category.create(req.body);
     res.status(200).json(newCategory)
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).json(err);
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
-  try{} catch (err) {res.status(500).json(err);}
+  try{
+    const updateCategory = await Category.update(req.body, {where: {id: req.params.id}});
+    res.status(200).json(updateCategory)
+  } catch (err) {res.status(400).json(err);}
 });
 
 router.delete('/:id', (req, res) => {
