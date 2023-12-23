e6ed389d79de52cf111780abadd83ad22a2dcae6
Raszero <travisjcelli95@gmail.com>
2023-12-23 13:15:28 -0600

    created get req for category:id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -15,25 +15,30 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
-  try{
-  } catch (err) {}
-  // find one category by its `id` value
+   // find one category by its `id` value
   // be sure to include its associated Products
+  try{
+    const categoryId = await Category.findByPk(req.params.id, {include: Product})
+    res.status(200).json(categoryId)
+  } catch (err) {
+    res.status(500).json(err);
+  }
+ 
 });
 
 router.post('/', (req, res) => {
   // create a new category
-  try{} catch (err) {}
+  try{} catch (err) {res.status(500).json(err);}
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
-  try{} catch (err) {}
+  try{} catch (err) {res.status(500).json(err);}
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
-  try{} catch (err) {}
+  try{} catch (err) {res.status(500).json(err);}
 });
 
 module.exports = router;
