e2a1f486e6cf2af80a3b67d342b8b908caee0c72
Raszero <travisjcelli95@gmail.com>
2023-12-23 14:50:10 -0600

    created put req for products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -28,16 +28,16 @@ router.get("/:id", async (req, res) => {
 });
 
 // create new product
-router.post("/", (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
+router.post("/", async (req, res) => {
+  try {
+    const createProduct = await Product.create(req.body);
+    res.status(200).json(createProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
   Product.create(req.body)
+
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
@@ -103,8 +103,9 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete one product by its `id` value
+  
 });
 
 module.exports = router;
