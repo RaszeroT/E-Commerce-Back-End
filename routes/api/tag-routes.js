ce302f3c7313da9696bff0e801e0856f9e23c55d
Raszero <travisjcelli95@gmail.com>
2023-12-23 15:32:31 -0600

    created post route for tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -25,8 +25,14 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new tag
+  try {
+    const createTag = await Tag.create(req.body)
+    res.status(200).json(createTag)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 router.put("/:id", (req, res) => {
